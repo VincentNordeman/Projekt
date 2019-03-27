0f5848ac7d41379409587335a98f629638805544
Vincent <vincent.nordeman@learnet.se>
2019-03-27 13:24:53 +0100

    Påbörjat hemsidan

--- favo.php
+++ favo.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Favorit</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <div class="container">
+
+        <header>
+            <h1>Skol Reviews</h1>
+            <nav>
+                <a href="#">Startsida</a>
+                <a href="#">Favorit Restauranger</a>
+                <a href="#">Skapa Konto</a>
+                <a href="#">Logga in</a>
+            </nav>
+        </header>
+
+        <main>
+            <section class="karta"></section>
+            <section class="favoLista"></section>
+        </main>
+    </div>
 </body>
+
 </html>
--- hem.php
+++ hem.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hem</title>
+    <title>Startsida</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <div class="container">
+
+        <header>
+            <h1>Skol Reviews</h1>
+            <nav>
+                <a href="#">Startsida</a>
+                <a href="#">Favorit Restauranger</a>
+                <a href="#">Skapa Konto</a>
+                <a href="#">Logga in</a>
+            </nav>
+        </header>
+
+        <main>
+            <section class="karta"></section>
+            <section class="review"></section>
+            <section class="dropdown">
+                <div class="drop">
+                    <button class="dropbtn">Dropdown</button>
+                    <div class="dropdown-content">
+                        <a href="#">Mamma Mia</a>
+                        <a href="#">La Grande</a>
+                        <a href="#">Subway</a>
+                        <a href="#">Hemköp</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
 </body>
+
 </html>
